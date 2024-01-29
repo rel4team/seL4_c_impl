64a548a0efa3c5b395e33b9cf94e10cdaa1afb4f
CtrlZ233 <328825619@qq.com>
2024-01-29 07:33:54 +0000

    debug

---
+++ kernel-settings.cmake
@@ -0,0 +1,16 @@
+#
+# Copyright 2023, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# Basis for seL4 kernel configuration
+
+set(KernelArch riscv64 CACHE STRING "")
+set(KernelMaxNumNodes 2 CACHE STRING "")
+set(KernelPlatform spike CACHE STRING "")
+set(KernelSel4Arch riscv64 CACHE STRING "")
+set(KernelVerificationBuild OFF CACHE BOOL "")
+set(KernelWordSize 64 CACHE STRING "")
+set(KernelWordSize 64 CACHE STRING "")
+set(KernelWordSize 64 CACHE STRING "")
