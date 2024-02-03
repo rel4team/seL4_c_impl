a503ed12aef606e101199c2b8512d74ab4bcc081
CtrlZ233 <328825619@qq.com>
2024-02-04 07:58:25 +0800

    debug

--- kernel-settings.cmake
+++ kernel-settings.cmake
@@ -7,10 +7,9 @@
 # Basis for seL4 kernel configuration
 
 set(KernelArch riscv64 CACHE STRING "")
-set(KernelMaxNumNodes 2 CACHE STRING "")
+set(KernelMaxNumNodes 4 CACHE STRING "")
+set(KernelIsUintr ON CACHE BOOL "")
 set(KernelPlatform spike CACHE STRING "")
 set(KernelSel4Arch riscv64 CACHE STRING "")
 set(KernelVerificationBuild OFF CACHE BOOL "")
-set(KernelWordSize 64 CACHE STRING "")
-set(KernelWordSize 64 CACHE STRING "")
 set(KernelWordSize 64 CACHE STRING "")
